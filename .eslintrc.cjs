f828b3e94f187aec4f53820d59c887578fd7591b
Vu Tran <vutranv@nashtechglobal.com>
2023-02-09 09:56:14 +0700

    Initial setup for vue-3

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'prettier/prettier': ['error', { semi: false }]
+  }
+}
